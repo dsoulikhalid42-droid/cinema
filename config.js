127ee4c41ba9ea030dba2f5d025a1904183d5c30
dsoulikhalid42-droid <dsoulikhalid42@gmail.com>
2026-06-26 18:59:13 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    BASE_URL: '/api/tmdb', // Calls Cloudflare Pages function natively
+    IMG_URL: 'https://image.tmdb.org/t/p/w500',
+    IMG_URL_ORIGINAL: 'https://image.tmdb.org/t/p/original'
+};
