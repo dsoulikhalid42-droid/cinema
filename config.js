8bacd818c81b3c9ab8360ca649f5a7fb92750b32
dsoulikhalid42-droid <dsoulikhalid42@gmail.com>
2026-06-26 19:56:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js
 const CONFIG = {
-    BASE_URL: '/api/tmdb', // Calls Cloudflare Pages function natively
+    BASE_URL: '/api/tmdb', 
     IMG_URL: 'https://image.tmdb.org/t/p/w500',
     IMG_URL_ORIGINAL: 'https://image.tmdb.org/t/p/original'
 };
