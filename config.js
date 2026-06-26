ce82be8059c961141bfa04dae2a65f4dc2606b18
dsoulikhalid42-droid <dsoulikhalid42@gmail.com>
2026-06-26 20:12:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    BASE_URL: '/api/tmdb', 
+    BASE_URL: 'https://api.themoviedb.org/3', // Points directly to TMDB
+    TMDB_API_KEY: 'PASTE_YOUR_ACTUAL_TMDB_API_KEY_HERE', // Put your secret key right here
     IMG_URL: 'https://image.tmdb.org/t/p/w500',
     IMG_URL_ORIGINAL: 'https://image.tmdb.org/t/p/original'
 };
